a52850bd212175f60c9d674b256291a3c1d8e1e2
Anastasiia Skvorsova <anastasiiaskv@gmail.com>
2018-03-20 17:26:16 +0200

    Second Commit

--- src/testapp/TestApp.java
+++ src/testapp/TestApp.java
@@ -5,17 +5,15 @@
  */
 package testapp;
 
-/**
- *
- * @author macbook
- */
 public class TestApp {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+/**
+* @param args the command line arguments
+*/
+public static void main(String[] args) {
+    // TODO code application logic here
+float a=10;
+float b=(float) 12.5;
+System.out.println("a+b="+(a+b));
+System.out.println("GIT test application");
+}
 }

880654a26dbcc7ed1ee6b55a13b9c7fec9de74ed
Anastasiia Skvorsova <anastasiiaskv@gmail.com>
2018-03-20 17:32:37 +0200

    Third commit

--- src/testapp/TestApp.java
+++ src/testapp/TestApp.java
@@ -5,15 +5,21 @@
  */
 package testapp;
 
+import java.util.Scanner;
+
 public class TestApp {
 /**
 * @param args the command line arguments
 */
 public static void main(String[] args) {
     // TODO code application logic here
-float a=10;
-float b=(float) 12.5;
+Scanner in = new Scanner(System.in);
+System.out.print("1st num: ");
+double a = Double.valueOf(in.nextLine());
+System.out.print("2nd num: ");
+double b = Double.valueOf(in.nextLine());
 System.out.println("a+b="+(a+b));
 System.out.println("GIT test application");
 }
 }
+
